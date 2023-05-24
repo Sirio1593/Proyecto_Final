4c6de24c2d9e93c43a27c9e637ba651e07a8940d
Sirio1593 <128339037+Sirio1593@users.noreply.github.com>
2023-05-24 19:05:58 -0300

    Agregado de tar Entrega

---
+++ Archivo.py
@@ -0,0 +1,4 @@
+nothing added to commit but untracked files present (use "git add" to track)
+PS C:\Users\maria\OneDrive\Documentos\Python\Proyecto Final> git remote add origin https://github.com/Sirio1593/Proyecto_Final.git
+PS C:\Users\maria\OneDrive\Documentos\Python\Proyecto Final> git branch -M main
+PS C:\Users\maria\OneDrive\Documentos\Python\Proyecto Final> git push -u origin main
